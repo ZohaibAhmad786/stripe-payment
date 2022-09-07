4a4c68b7f5e728d67c42ec1734b70737bd856b98
Hafiz.fahad <hafiz.fahad@admiraldigital.dev>
2022-09-07 12:58:41 +0500

    env-config: testing eslint and precommit hook

--- .prettierrc.js
+++ .prettierrc.js
@@ -1,7 +1,10 @@
 module.exports = {
   arrowParens: 'avoid',
   bracketSameLine: true,
-  bracketSpacing: false,
   singleQuote: true,
   trailingComma: 'all',
+  bracketSpacing: true,
+  tabWidth: 2,
+  printWidth: 150,
+
 };
--- ios/latestApp/AppDelegate.mm
+++ ios/latestApp/AppDelegate.mm
@@ -3,6 +3,7 @@
 #import <React/RCTBridge.h>
 #import <React/RCTBundleURLProvider.h>
 #import <React/RCTRootView.h>
+#import <GoogleMaps/GoogleMaps.h>
 
 #import <React/RCTAppSetupUtils.h>
 
@@ -31,6 +32,7 @@ static NSString *const kRNConcurrentRoot = @"concurrentRoot";
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
 {
+  [GMSServices provideAPIKey:@"AIzaSyDCNdxZGdKj6xJLhYw_s550yunn-mjq6K8"];
   RCTAppSetupPrepareApp(application);
 
   RCTBridge *bridge = [[RCTBridge alloc] initWithDelegate:self launchOptions:launchOptions];
