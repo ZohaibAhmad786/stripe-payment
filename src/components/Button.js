93f814236a6196a94855d931fc473757fcac796e
Hafiz.fahad <hafiz.fahad@admiraldigital.dev>
2022-09-07 13:04:32 +0500

    env-config: testing eslint and precommit hook

--- src/components/Button.js
+++ src/components/Button.js
@@ -1,13 +1,6 @@
-import React, { PureComponent } from 'react'
-import {
-  View,
-  Text,
-  TouchableHighlight,
-  ActivityIndicator,
-  Platform,
-  StyleSheet,
-} from 'react-native'
-import PropTypes from 'prop-types'
+import React, { PureComponent } from 'react';
+import { View, Text, TouchableHighlight, ActivityIndicator, Platform, StyleSheet } from 'react-native';
+import PropTypes from 'prop-types';
 
 export default class Button extends PureComponent {
   static propTypes = {
@@ -17,44 +10,40 @@ export default class Button extends PureComponent {
     disabled: PropTypes.bool,
     style: PropTypes.any,
     onPress: PropTypes.func.isRequired,
-  }
+  };
 
   static defaultProps = {
     disabledText: '',
     loading: false,
     disabled: false,
     style: undefined,
-  }
+  };
 
-  handlePress = (event) => {
-    const { loading, disabled, onPress } = this.props
+  handlePress = event => {
+    const { loading, disabled, onPress } = this.props;
 
     if (loading || disabled) {
-      return
+      return;
     }
 
     if (onPress) {
-      onPress(event)
+      onPress(event);
     }
-  }
+  };
 
   render() {
-    const { text, disabledText, loading, disabled, style, ...rest } = this.props
+    const { text, disabledText, loading, disabled, style, ...rest } = this.props;
 
     return (
-      <TouchableHighlight
-        {...rest}
-        style={[styles.button, style]}
-        underlayColor="rgba(0,0,0,0.5)"
-        onPress={this.handlePress}
-      >
+      <TouchableHighlight {...rest} style={[styles.button, style]} underlayColor="rgba(0,0,0,0.5)" onPress={this.handlePress}>
         <View>
           {loading && <ActivityIndicator animating size="small" />}
           {!loading && !disabled && <Text>{text}</Text>}
+
           {!loading && disabled && <Text>{disabledText || text}</Text>}
         </View>
       </TouchableHighlight>
-    )
+    );
   }
 }
 
@@ -70,4 +59,4 @@ const styles = StyleSheet.create({
     backgroundColor: 'white',
     alignItems: 'center',
   },
-})
+});
